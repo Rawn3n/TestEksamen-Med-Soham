1452a989c73a8d29aba94aa535b88288d6c0cdc2
Rawn3n <129275096+Rawn3n@users.noreply.github.com>
2026-01-13 11:22:07 +0100

    ss

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 
 public class PlayerSetup : NetworkBehaviour
 {
-    [Header("Local-only objects")]
+    [Header("Ignore")]
     [SerializeField] GameObject playerCamera;
     [SerializeField] GameObject crosshairCanvas;
+    [SerializeField] AudioListener audioListener;
 
     protected override void OnSpawned()
     {
-        if (isOwner)
-            return;
-
-        // Disable everything that should only exist locally
-        playerCamera.SetActive(false);
-        crosshairCanvas.SetActive(false);
+        if (!isOwner)
+        {
+            // Disable alle objekter det kun skal ses lokalt
+            playerCamera.SetActive(false);
+            crosshairCanvas.SetActive(false);
+            audioListener.enabled = false;
+        }
     }
 }
